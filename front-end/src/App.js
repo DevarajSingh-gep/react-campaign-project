891a0d91c2b54129acf4544f60e815175b9ecc3b
Devaraj Singh <Devaraj.Singh@gep.com>
2021-03-13 18:53:39 +0530

    +

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Container from './Container/Container';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
@@ -36,71 +37,22 @@ class App extends Component {
                   name: 'Action'
               }
           ],
-          data:[{
-              "id": 1,
-              "name": "Test Whatsapp",
-              "region": "US",
-              "createdOn": 1615982744000,
-              "price": "Price info of Test Whatsapp",
-              "csv": "Some CSV link for Whatsapp",
-              "report": "Some report link for Whatsapp",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80" 
-            },
-            {
-              "id": 2,
-              "name": "Super Jewels Quest",
-              "region": "CA, FR",
-              "createdOn": 1615982744000,
-              "price": "Price info of Super Jewels Quest",
-              "csv": "Some CSV link for Super Jewels Quest",
-              "report": "Some report link for Super Jewels Ques",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
-            },
-            {
-              "id": 3,
-              "name": "Mole Slayer",
-              "region": "FR",
-              "createdOn": 1615982744000,
-              "price": "Price info of Mole Slayer",
-              "csv": "Some CSV link for Mole Slayer",
-              "report": "Some report link for Mole Slayer",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
-            },
-            {
-              "id": 4,
-              "name": "Mancala Mix",
-              "region": "JP",
-              "createdOn": 1610280344000,
-              "price": "Price info of Mancala Mix",
-              "csv": "Some CSV link for Mancala Mix",
-              "report": "Some report link for Mancala Mix",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
-            },
-            {
-              "id": 5,
-              "name": "Mancala Mix",
-              "region": "JP",
-              "createdOn": 1610885144000,
-              "price": "Price info of Mancala Mix",
-              "csv": "Some CSV link for Mancala Mix",
-              "report": "Some report link for Mancala Mix",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
-            },
-            {
-              "id": 6,
-              "name": "Mancala Mix",
-              "region": "JP",
-              "createdOn": 1615377944000,
-              "price": "Price info of Mancala Mix",
-              "csv": "Some CSV link for Mancala Mix",
-              "report": "Some report link for Mancala Mix",
-              "image_url":"https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80"
-            }
-          ]
+          data:null
       },
       currentDate: new Date().getTime()
     }
     this.activeIndex = 0;
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount Run');
+    axios.get('/api/campaignData').then((res)=>{
+      let {tableData} = this.state;
+      tableData.data = res.data;
+      this.setState({
+        tableData: tableData
+      })
+    })
   }
 
   tabHandlerClick = (selectedtab, index) => {
@@ -171,10 +123,10 @@ class App extends Component {
 
     return (
       <div>
-        <Container tabData={this.state.tabs} 
+        {this.state.tableData.data != null ?<Container tabData={this.state.tabs} 
         tableConfig={this.getDataAccordingToCampaign()} 
         tabClickEvent={this.tabHandlerClick}
-        campaignHandler={this.campaignHandler}></Container>
+        campaignHandler={this.campaignHandler}></Container>: "Sorry Wait for a moment....."}
       </div>
     );
   }
